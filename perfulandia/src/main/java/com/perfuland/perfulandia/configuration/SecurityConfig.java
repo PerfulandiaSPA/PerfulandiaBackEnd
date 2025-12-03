bebd952e90cbf99eb6dec61e12349c667f39e205
DebugggMax <max.olguin@duocuc.cl>
2025-12-03 02:03:45 -0300

    Funcionando al 100% backend en puntosegurogit

--- perfulandia/src/main/java/com/perfuland/perfulandia/configuration/SecurityConfig.java
+++ perfulandia/src/main/java/com/perfuland/perfulandia/configuration/SecurityConfig.java
@@ -9,39 +9,32 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/estado-prendas/**")
-                        .hasAnyAuthority("ROLE_TEST")
-                        .requestMatchers("/api/prendas/**")
-                        .hasAnyAuthority("ROLE_TEST")
-                        .requestMatchers("/api/tipo-prendas/**")
-                        .hasAnyAuthority("ROLE_TEST") // Permitir acceso a
-                                                      // usuarios con rol
-                                                      // Tester
-                        .requestMatchers("/api/auth/login").permitAll() // Permitir acceso libre
-                                                                        // al endpoint de login
-                        .requestMatchers("/api/usuarios/**")
-                        .hasAnyAuthority("ROLE_ADMIN")
-                        // Permisos para el SWAGGER UI
-                        .requestMatchers("/doc/swagger-ui/**",
-                                "/doc/swagger-ui/index.html",
-                                "/doc/swagger-ui/swagger-ui.css",
-                                "/doc/swagger-ui/swagger-ui-bundle.js",
-                                "/doc/swagger-ui/swagger-ui-standalone-preset.js",
-                                "/doc/swagger-ui/index.css",
-                                "/doc/swagger-ui/swagger-initializer.js",
-                                "/doc/swagger-ui/favicon-32x32.png",
-                                "/v3/api-docs/swagger-config",
-                                "/v3/api-docs")
-                        .permitAll() // Permitir acceso libre al endpoint de swagger
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                return http
+                                .csrf(csrf -> csrf.disable())
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/api/v1/perfumes/**")
+                                                .hasAnyAuthority("ROLE_WORKER")
+                                                .requestMatchers("/api/v1/orders/**")
+                                                .hasAnyAuthority("ROLE_WORKER")
+                                                .requestMatchers("/api/v1/categories/**")
+                                                .hasAnyAuthority("ROLE_WORKER") // Permitir acceso a
+                                                                                // usuarios con rol
+                                                                                // WORKER, ADMIN o CLIENT
+                                                .requestMatchers("/api/auth/login").permitAll() // Permitir acceso libre
+                                                                                                // al endpoint de login
+                                                .requestMatchers("/api/v1/users/**")
+                                                .hasAnyAuthority("ROLE_ADMIN")
+                                                // Permisos para el SWAGGER UI
+                                                .requestMatchers("/v3/api-docs/**",
+                                                                "/swagger-ui.html",
+                                                                "/swagger-ui/**")
+                                                .permitAll() // Permitir acceso libre al endpoint de swagger
 
-                        .anyRequest().authenticated())
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+                                                .anyRequest().authenticated())
+                                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .build();
         }
-                
+
 }
