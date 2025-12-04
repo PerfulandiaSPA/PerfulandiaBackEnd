dd7d74c4487a13ef564f04dc6ac77902083ccdd3
moi.rodriguez <moi.rodriguez@duocuc.cl>
2025-12-03 23:01:06 -0300

    fix: update security configuration to allow public access to specific endpoints

--- perfulandia/src/main/java/com/perfuland/perfulandia/configuration/SecurityConfig.java
+++ perfulandia/src/main/java/com/perfuland/perfulandia/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.perfuland.perfulandia.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,28 +10,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-        @Bean
+@Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
                 return http
                                 .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers("/api/v1/perfumes/**")
-                                                .hasAnyAuthority("ROLE_WORKER")
-                                                .requestMatchers("/api/v1/orders/**")
-                                                .hasAnyAuthority("ROLE_WORKER")
-                                                .requestMatchers("/api/v1/categories/**")
-                                                .hasAnyAuthority("ROLE_WORKER") // Permitir acceso a
-                                                                                // usuarios con rol
-                                                                                // WORKER, ADMIN o CLIENT
-                                                .requestMatchers("/api/auth/login").permitAll() // Permitir acceso libre
-                                                                                                // al endpoint de login
-                                                .requestMatchers("/api/v1/users/**")
-                                                .hasAnyAuthority("ROLE_ADMIN")
+                                                .requestMatchers(HttpMethod.GET, "/api/v1/perfumes/**").permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/api/v1/users/**").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll() 
+
+                                                //.requestMatchers("/api/v1/orders/**").hasAnyAuthority("ROLE_WORKER")
+                                                //.requestMatchers("/api/v1/categories/**").permitAll()
+
                                                 // Permisos para el SWAGGER UI
                                                 .requestMatchers("/v3/api-docs/**",
                                                                 "/swagger-ui.html",
                                                                 "/swagger-ui/**")
-                                                .permitAll() // Permitir acceso libre al endpoint de swagger
+                                                .permitAll() 
 
                                                 .anyRequest().authenticated())
                                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
